82771a0f2bf1969dab07b03a9dfc5f930d1c549a
acronicsM <91975262+acronicsM@users.noreply.github.com>
2021-11-04 10:12:08 +0300

    исправил

--- Lesson3/lesson3_6.py
+++ Lesson3/lesson3_6.py
@@ -1,38 +1,38 @@
 from random import randint as rnd
 
-nouns = ["автомобиль", "лес", "огонь", "город", "дом"]
-adverbs = ["сегодня", "вчера", "завтра", "позавчера", "ночью"]
-adjectives = ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]
-
 
 def get_jokes(number_jokes, nouns_n, adverbs_n, adjectives_n, flag=True):
     jokes = []
+    nouns_n_sh = list(range(0, len(nouns_n)))
+    adverbs_n_sh = list(range(0, len(adverbs_n)))
+    adjectives_n_sh = list(range(0, len(adjectives_n)))
+
     for i in range(number_jokes):
-        len_nouns = len(nouns_n) - 1
-        len_adverbs = len(adverbs_n) - 1
-        len_adjectives = len(adjectives_n) - 1
+        len_nouns = len(nouns_n_sh)
+        len_adverbs = len(adverbs_n_sh)
+        len_adjectives = len(adjectives_n_sh)
 
         if len_nouns > 0:
             index_nouns = rnd(0, len_nouns-1)
-            jokes_nouns = nouns_n[index_nouns]
+            jokes_nouns = nouns_n[nouns_n_sh[index_nouns]]
             if flag:
-                nouns_n.pop(index_nouns)
+                nouns_n_sh.pop(index_nouns)
         else:
             jokes_nouns = ''
 
         if len_adverbs > 0:
             index_adverbs = rnd(0, len_adverbs - 1)
-            jokes_adverbs = adverbs_n[index_adverbs]
+            jokes_adverbs = adverbs_n[adverbs_n_sh[index_adverbs]]
             if flag:
-                adverbs_n.pop(index_adverbs)
+                adverbs_n_sh.pop(index_adverbs)
         else:
             jokes_adverbs = ''
 
         if len_nouns > 0:
             index_adjectives = rnd(0, len_adjectives - 1)
-            jokes_adjectives = adjectives_n[index_adjectives]
+            jokes_adjectives = adjectives_n[adjectives_n_sh[index_adjectives]]
             if flag:
-                adjectives_n.pop(index_adjectives)
+                adjectives_n_sh.pop(index_adjectives)
         else:
             jokes_adjectives = ''
 
@@ -41,9 +41,7 @@ def get_jokes(number_jokes, nouns_n, adverbs_n, adjectives_n, flag=True):
     return jokes
 
 
-print(get_jokes(7, nouns, adverbs, adjectives))
-
-nouns = ["автомобиль", "лес", "огонь", "город", "дом"]
-adverbs = ["сегодня", "вчера", "завтра", "позавчера", "ночью"]
-adjectives = ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]
-print(get_jokes(7, nouns, adverbs, adjectives, False))
+print(get_jokes(7, ["автомобиль", "лес", "огонь", "город", "дом"], ["сегодня", "вчера", "завтра", "позавчера", "ночью"],
+                ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]))
+print(get_jokes(7, ["автомобиль", "лес", "огонь", "город", "дом"], ["сегодня", "вчера", "завтра", "позавчера", "ночью"],
+                ["веселый", "яркий", "зеленый", "утопичный", "мягкий"], False))

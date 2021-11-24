6f062bec08120fd6e5ad27fb3a07f07c8f9f3655
MikeParamonov <kloze-spb@mal.ru>
2021-11-24 19:29:30 +0300

    ПЗ9

---
+++ Lesson9/L9_5.py
@@ -0,0 +1,30 @@
+class Stationery:
+    def __init__(self, title):
+        self.title = title
+
+    def draw(self):
+        print('Запуск отрисовки')
+
+
+class Pen(Stationery):
+    def draw(self):
+        print('Запуск отрисовки ручкой')
+
+
+class Pencil(Stationery):
+    def draw(self):
+        print('Запуск отрисовки карандашом')
+
+
+class Handle(Stationery):
+    def draw(self):
+        print('Запуск отрисовки маркером')
+
+
+p1 = Pen('карандаш')
+pl1 = Pencil('ручка')
+h1 = Handle('маркер')
+
+p1.draw()
+pl1.draw()
+h1.draw()
